7f1707d0433ad287865a8f4b93a5cd31396e3dc6
Ngo Jun Hao Jason <njhjason@protonmail.com>
2024-08-07 13:41:46 +1200

    Add introduction to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,51 @@
 "use client";
 import Image from "next/image";
 import PageContent from "./ui/PageContent";
+import styled from "styled-components";
 
 const LandingPage = () => {
   return (
-    <PageContent>
+    <LandingPageContent>
       <MainBanner />
-    </PageContent>
+
+      <Introduction>
+        <Title>Welcome to Kakapo Lodge</Title>
+
+        <p>
+          The two-story purpose-built backpacker hostel features modern rooms,
+          an open lounge with warm fireplace, and a relaxed, friendly
+          atmosphere. Enjoy our spacious, well-appointed backpacker
+          accommodation, Netflix, fast unlimited WiFi, large kitchen, outdoor
+          courtyard with views of the Southern Alps.
+        </p>
+
+        <p>
+          Just three minutes' walk from the town's main attraction, Hanmer
+          Springs Thermal Pools and Spa, and a pleasant stroll to numerous
+          restaurants and stores, our hostel is a great base to enjoy your
+          relaxing Hanmer getaway.
+        </p>
+
+        <p>
+          All ensuite accommodation has free towels as well as free soap and
+          shampoo.
+        </p>
+      </Introduction>
+    </LandingPageContent>
   );
-}
+};
 
 export default LandingPage;
+
+const LandingPageContent = styled(PageContent)`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  @media (width < 700px) {
+    gap: 16px;
+  }
+`;
 
 const MainBanner = () => {
   return (
@@ -24,3 +59,25 @@ const MainBanner = () => {
     />
   );
 };
+
+const Introduction = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  padding: 0px 16vw;
+
+  @media (width < 700px) {
+    padding: 0px 32px;
+  }
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: xx-large;
+  font-weight: 500;
+
+  @media (width < 700px) {
+    font-size: x-large;
+  }
+`;
