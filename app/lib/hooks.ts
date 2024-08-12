5eebdbf6314f71cc67e8d374dd5733d500e0d244
Ngo Jun Hao Jason <njhjason@protonmail.com>
2024-08-12 17:24:29 +1200

    Fix NextJS undefined document error

--- app/lib/hooks.ts
+++ app/lib/hooks.ts
@@ -4,8 +4,8 @@ const MOBILE_MAX_WIDTH = 768;
 
 // reference: https://stackoverflow.com/a/64218472
 export const useIsMobile = () => {
-  const [width, setWidth] = useState(document.documentElement.clientWidth);
-  const updateWidth = () => setWidth(document.documentElement.clientWidth);
+  const [width, setWidth] = useState(getWidth());
+  const updateWidth = () => setWidth(getWidth());
 
   useEffect(() => {
     window.onresize = updateWidth;
@@ -15,5 +15,11 @@ export const useIsMobile = () => {
     };
   }, []);
 
-  return width < MOBILE_MAX_WIDTH;
+  return width <= MOBILE_MAX_WIDTH;
+};
+
+const getWidth = () => {
+  return typeof document === "undefined"
+    ? MOBILE_MAX_WIDTH
+    : document.documentElement.clientWidth;
 };
